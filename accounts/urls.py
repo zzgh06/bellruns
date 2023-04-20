ab85526acb8f052a8dfec99f0c0c0d7336d3656a
zzgh06 <zzgh06@naver.com>
2023-04-20 14:14:57 +0900

    feature/accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('posts:index')
+    
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('posts:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+def logout(request):
+    if request.user.is_authenticated:
+        auth_logout(request)
+    return redirect('posts:index')
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('posts:index')
+    
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('posts:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'posts/login.html', context)
