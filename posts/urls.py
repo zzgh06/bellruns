f3da73d7e2e81d55e8d784f0ab4e413071a58ac3
zzgh06 <zzgh06@naver.com>
2023-04-20 14:49:37 +0900

    feature/posts

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 app_name = 'posts'
 urlpatterns = [
-    
+    path('', views.index, name='index'),
+    path('<int:post_pk>/', views.detail, name='detail'),
+    path('create/', views.create, name='create'),
+    path('<int:post_pk>/delete/', views.delete, name='delete'),
 ]
