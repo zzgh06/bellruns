ed9a18df1d0e6fca06794f89647e44669bb87c3e
zzgh06 <zzgh06@naver.com>
2023-04-21 14:03:33 +0900

    feature/balancegame

--- posts/urls.py
+++ posts/urls.py
@@ -5,6 +5,6 @@ app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:post_pk>/', views.detail, name='detail'),
-    path('create/', views.create, name='create'),
+    path('create/', views.create_post, name='create_post'),
     path('<int:post_pk>/delete/', views.delete, name='delete'),
 ]
